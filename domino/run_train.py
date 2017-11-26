f7f3e26f549511d72f2e17a641a968601885f3b1
Kate Park <kate.of.park@gmail.com>
2017-11-25 18:25:45 -0800

    time

--- domino/run_train.py
+++ domino/run_train.py
@@ -1,11 +1,15 @@
 from agent import Agent
+import time
 
 def main():
     agent = Agent()
 
-    num_games = 200
+    num_games = 50
+    start = time.time()
     agent.selfplay(num_games)
     agent.train()
+    end = time.time()
+    print('time to train', end - start)
     agent.playGreedy(num_games)
 
 if __name__ == '__main__':
