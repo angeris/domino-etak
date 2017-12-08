4af4d7632cdf3c0c2c4339b22e2857be9f0f90a8
Guillermo Angeris <guillean@stanford.edu>
2017-12-08 15:41:56 -0800

    tiny for fun

--- domino/featureagent.py
+++ domino/featureagent.py
@@ -10,3 +10,17 @@ class FeatureAgent:
     def save_weights(self):
         pass
 
+    def last_k_pip(self, game, player, move):
+        remaining_dominoes = 7
+        curr_domino = move[0]
+        for d in game.board:
+            if move.fits_val(d):
+                remaining_dominoes -= 1
+
+        for d in game.get_player_hand(player):
+            if d == move:
+                continue
+            if move.fits_val(d):
+                remaining_dominoes -= 1
+
+        return remaining_dominoes == 1
